bd4b1a9bfcbb654166a1760b8810b85c5ebbae3b
ZeynepKst <zzeynepkustu@gmail.com>
2022-10-31 15:35:03 +0300

    js new

---
+++ Temeller/console/var,let,const/app.js
@@ -0,0 +1,30 @@
+// var a;
+// a=20;
+// console.log(a);
+
+// var name=" mustafa";
+// var name="murat";
+
+// console.log(name);
+
+
+// let anahtar kelimesi
+
+// let name= "mustafa murat";
+// console.log(name);
+
+// name= "murat";
+// console.log(name);
+
+// let a,b;
+// a=10;
+// b=20;
+// console.log(a+b);
+
+// const
+
+// const a=[1,2,3,4,5];
+// console.log(a);
+// a.push(6);
+// console.log(a);
+
