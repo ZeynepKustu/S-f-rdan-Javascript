64f252c215cefc2746e8cfaf7f4d9a2ea755ccfb
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-01 14:57:18 +0300

    scope

---
+++ Temeller/scope/app.js
@@ -0,0 +1,48 @@
+//scope kavramı
+
+var value1 = 10;
+let value2 = 20;
+const value3 = 30;
+
+// function func() {
+//     var value1 = 40;
+//     let value2 = 50;
+//     const value3 = 60;
+
+//     console.log(value1, value2, value3);
+// }
+// func();
+
+
+
+
+if(true){
+    var value1=40;
+    let value2=50;
+    const value3=60;
+console.log(value1,value2,value3);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(value1, value2, value3);
+
+
+
+
+
+//global scope
+
+
+
+
+
+// function a(){
+//     //function scope
+// }
+
+// if(){
+//     //block scope
+// }
