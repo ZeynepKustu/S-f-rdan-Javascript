69737da0212c5fb0219747fcd1b705eabe3b4434
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-01 12:34:40 +0300

    kosullar

---
+++ Temeller/koşullu durumlar/app.js
@@ -0,0 +1,70 @@
+// karşılaştırma operatörleri
+
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+// console.log(2==2);
+// console.log("js"=="java");
+
+// console.log(2=="2");  //biri string biri number ise otomatik stringe çevirir
+
+// console.log(2==="2");
+// console.log(2 != 2);
+
+//not operatörü
+// console.log(!(2==2));
+
+//and operatörü
+// console.log((2==2)&&("ahmet"=="zeynep"))
+
+//or operatörü
+// console.log((2==2)||("ahmet"=="zeynep"))
+
+// if(koşul){
+//     if block
+// }
+
+
+// const error = true;
+// if(error == true){
+//     console.log("hata oluştu");
+// }
+// else{
+//     console.log("hata oluşmadı");
+// }
+
+
+// const user= "mms";
+// if(user==="mmc"){
+//     console.log("kullanıcı bulundu");
+// }
+// else{
+//     console.log("kullanıcı bulunmadı");
+// }
+
+
+
+// const process="5";
+// if(process==="1"){
+//     console.log("işlem 1");
+// }
+// else if(process ==="2"){
+//     console.log("işlem 2");
+// }
+// else if(process ==="3"){
+//     console.log("işlem 3");
+// }
+// else{
+//     console.log("geçersiz işlem");
+// }
+
+
+//ternary operatör
+const number=100;
+console.log(number===100 ? "sayı 100" : "sayı 100 değil");
