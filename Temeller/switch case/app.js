0c4af5f1fd1484082449ddee7b7786553784e05d
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-01 12:42:04 +0300

    switch-case

---
+++ Temeller/switch case/app.js
@@ -0,0 +1,32 @@
+// const process = "5";
+// if (process === "1") {
+//     console.log("işlem 1");
+// }
+// else if (process === "2") {
+//     console.log("işlem 2");
+// }
+// else if (process === "3") {
+//     console.log("işlem 3");
+// }
+// else {
+//     console.log("geçersiz işlem");
+// }
+
+
+const process = 3;
+
+switch (process) {
+    case 1:
+        console.log("işlem 1");
+        break;
+    case 2:
+        console.log("işlem 2");
+        break;
+    case 3:
+        console.log("işlem 3");
+        break;
+    default:
+        console.log("geçersiz işlem");
+        break;
+
+}
