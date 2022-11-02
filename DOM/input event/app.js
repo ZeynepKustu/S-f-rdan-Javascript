582873629ab9aad7bc567002d9b1be6e78da867b
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-02 15:13:41 +0300

    input event

---
+++ DOM/input event/app.js
@@ -0,0 +1,33 @@
+const filter= document.getElementById("filter");
+
+// document.addEventListener("DOMContentLoaded",load);
+
+// function load(e){
+//     console.log("sayfa yüklendi");
+// }
+
+
+// //focus
+// filter.addEventListener("focus",run);
+// //blur
+// filter.addEventListener("blur",run);
+
+// //paste
+// filter.addEventListener("paste",run);
+
+// //copy
+// filter.addEventListener("copy",run);
+
+//cut
+// filter.addEventListener("cut",run);
+
+//select
+// filter.addEventListener("select",run);
+
+
+
+function run(e){
+    console.log(e.type);
+}
+
+
