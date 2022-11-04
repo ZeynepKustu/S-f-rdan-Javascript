fa1b0c169ca9d93295c3a6e1edfb653f3658f87e
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-04 13:35:55 +0300

    referans-primitive tipler

---
+++ ES6+ özellikleri/referans tipler ve primitive/app.js
@@ -0,0 +1,59 @@
+//primitive (değerler kıyaslanıyor)
+// let a="mustafa";
+// let b="mustafa";
+
+// if(a===b) {
+//     console.log("eşit");
+// }
+
+
+//referans (referanslar yani bellekteki yeri kıyaslanıyor)
+
+// let array1= [1,2,3,4,5];
+// let array2= [1,2,3,4,5];
+
+// if(array1 === array2){
+//     console.log("eşit");
+// }
+
+
+// const person1 = {
+//     name:"mustafa",
+//     age:25
+// }
+// const person2 = {
+//     name:"mustafa",
+//     age:25
+// }
+
+// if(person1 === person2) {
+//     console.log("eşit");
+// }
+
+
+
+// const cities = new Map();
+
+// cities.set("ankara",5);
+// cities.set("istanbul",15);
+
+// console.log(cities.get("ankara"));
+
+
+
+// const cities = new Map();
+
+// cities.set("ankara",5);
+// cities.set("istanbul",15);
+// cities.set([1,2,3],"Array");
+
+// console.log(cities.get([1,2,3]));
+
+
+// const cities = new Map();
+// const key= [1,2,3];
+// cities.set("ankara",5);
+// cities.set("istanbul",15);
+// cities.set(key,"Array");
+
+// console.log(cities.get(key));
