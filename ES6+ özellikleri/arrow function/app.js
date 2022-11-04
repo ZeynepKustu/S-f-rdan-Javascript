fcf91b157d409f4734a0f72129d654af0068f7c6
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-04 11:56:42 +0300

    arrow function

---
+++ ES6+ özellikleri/arrow function/app.js
@@ -0,0 +1,38 @@
+// const merhaba=function(){
+//     console.log("merhaba");
+// }
+// merhaba();
+
+//arrow function
+
+// const merhaba= () => {
+//     console.log("merhaba");
+// }
+// merhaba();
+
+// const merhaba = (firstName)=> {
+//     console.log("merhaba",firstName);
+// }
+// merhaba("Murat","Coşkun");
+
+//tek parametre gönderiyorsak daha kısa yazabiliriz
+
+// const merhaba = firstName => console.log("merhaba",firstName);
+// merhaba("murat");
+
+
+// const cube= function(x){
+//     return x*x*x ;
+// }
+// console.log(cube(4));
+
+
+// const cube = x => {
+//     return x*x*x;
+// };
+// console.log(cube(4));
+
+//tek bir işlem yaptğında şu şekilde kısaltılabilir
+
+const cube = x => x * x * x;
+console.log(cube(4));
