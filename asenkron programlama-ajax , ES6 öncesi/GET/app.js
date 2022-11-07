5b431f77272f0552c944abeaee98f588d8853424
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-07 20:30:23 +0300

    post-put

--- asenkron programlama-ajax , ES6 öncesi/GET/app.js
+++ asenkron programlama-ajax , ES6 öncesi/GET/app.js
@@ -23,6 +23,39 @@ class Request{
 
      this.xhr.send();
   }
+  post(url,data,callback){
+
+      this.xhr.open("POST",url);
+      this.xhr.setRequestHeader("Content-type","application/json");
+      
+      this.xhr.onload = () => {
+
+        if(this.xhr.status=== 201){
+            callback(null,this.xhr.responseText);
+        }
+        else {
+            callback("herhangi bir hata oluştu",null);
+        }
+      }
+      this.xhr.send(JSON.stringify(data));
+  }
+  put(url,data,callback){
+
+    this.xhr.open("PUT",url);
+    this.xhr.setRequestHeader("Content-type","application/json");
+    
+    this.xhr.onload = () => {
+
+      if(this.xhr.status=== 200){
+          callback(null,this.xhr.responseText);
+      }
+      else {
+          callback("herhangi bir hata oluştu",null);
+      }
+    }
+    this.xhr.send(JSON.stringify(data));
+}
+
 
 }
 const request = new Request();
@@ -36,13 +69,43 @@ const request = new Request();
 
 // });
 
-request.get("https://jsonplaceholder.typicode.com/albums/51",function(err,response){
- if (err===null){
-    console.log(response);
- }
- else{
+// request.get("https://jsonplaceholder.typicode.com/albums/51",function(err,response){
+//  if (err===null){
+//     console.log(response);
+//  }
+//  else{
+//     console.log(err);
+//  }
+
+// });
+
+// request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,title:"thriller"},function(err,album){
+
+//    if(err===null){
+//        console.log(album);
+
+//    }
+//    else{
+//     console.log(err);
+//    }
+
+
+
+// });
+
+
+request.put("https://jsonplaceholder.typicode.com/albums/10",{userId:142,title:"tarkan karma"},function(err,album){
+
+   if(err===null){
+       console.log(album);
+
+   }
+   else{
     console.log(err);
- }
+   }
+
+
 
 });
+
 
