760927ad264fbd39b3ebce4bac98fa7676554762
ZeynepKst <zzeynepkustu@gmail.com>
2022-11-08 18:02:46 +0300

    fetch-Get

---
+++ asenkron programlama-ajax , ES6 öncesi/Fetch-GET/app.js
@@ -0,0 +1,21 @@
+class Request {
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+
+        })
+
+    }
+}
+const request = new Request();
+let albums;
+request.get("https://jsonplaceholder.typicode.com/albums")
+
+.then(albums => {
+
+    console.log(albums);
+})
+    .catch(err => console.log(err));
